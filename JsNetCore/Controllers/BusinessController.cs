5cbc60a06ef3f87b6fa2cb372b5e61cc1debd321
danilovcpp <danilov.cpp@gmail.com>
2018-01-09 11:35:44 +0600

    Add model

--- JsNetCore/Controllers/BusinessController.cs
+++ JsNetCore/Controllers/BusinessController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Jint;
+using JsNetCore.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,27 +14,25 @@ namespace JsNetCore.Controllers
 	public class BusinessController : Controller
 	{
 		private readonly Engine _engine = new Engine();
-
+		private readonly Context _context = new Context();
+		
 		// GET: api/business/method1?a=1&b=2
 		[HttpGet("{method}", Name = "Get")]
 		public string Get(string method)
 		{
 			var script = System.IO.File.ReadAllText($"Scripts/{method}.js");
 			
-			//_engine.SetValue("log", new Action<object>(Console.WriteLine));
+			_engine.SetValue("exec", new Func<string, string>(_context.Exec));
 			
-			/*_engine.Execute(@"
-				function hello() {
-					log('Hello World');
-				};
-				hello();");*/
+			_engine.Execute(@"var result = exec('Hello World');");
+			var result = _engine.GetValue("result");
 			
 			foreach (var param in HttpContext.Request.Query)
 			{
 				Console.WriteLine(param);
 			}
 
-			return method.ToString() + script;
+			return result.ToString();
 		}
 	}
 }
---
+++ JsNetCore/Models/Context.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JsNetCore.Models
+{
+	public class Context
+	{
+		public string Exec(string query)
+		{
+			return query;
+		}
+	}
+}
