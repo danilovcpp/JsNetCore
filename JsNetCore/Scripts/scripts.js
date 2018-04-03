3d17b5fd8bcbcb0daadd58b1f91f08c827e69c2f
Alexey Vasilev <valexey.dev@gmail.com>
2018-04-03 12:31:56 +0600

    вынес скрипты в общий файл
    теперь все скрипты находятся в одном файле, а запуск осуществляется => образом devices.insert()

---
+++ JsNetCore/Scripts/scripts.js
@@ -0,0 +1,25 @@
+﻿var devices = {
+    insert: function (params) {
+        if (params.recId == null || params.recName == null)
+            return false;
+
+        return ExecSqlProcedure("Insert", "devices", params);
+    },
+
+    findbyrecid: function (params) {
+        return ExecSqlProcedure("findbyid", "devices", params.recId);
+    }
+}
+
+var tracks = {
+    insert: function (params) {
+        if (params.recId == null)
+            return false;
+
+        return ExecSqlProcedure("Insert", "tracks", params);
+    },
+
+    findbyrecid: function (params) {
+        return ExecSqlProcedure("findbyid", "tracks", params.recId);
+    }
+}
