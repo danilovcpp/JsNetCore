8eb85c6f27008a001e781cc35ceb52727cdfe9c2
Alexey Vasilev <valexey.dev@gmail.com>
2018-05-15 11:58:44 +0600

    добавил класс файловой модели

---
+++ JsNetCore/Models/FileModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JsNetCore.Models
+{
+	public class FileModel
+	{
+		public Guid LocalFileName { get; set; }
+		public string FileName { get; set; }
+		public string Path { get; set; }
+	}
+}
